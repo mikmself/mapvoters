0f981e27291385cf8fc7eb2d9f6ebc63ebcbbf27
mikmself <mikmself@gmail.com>
2024-05-14 21:37:55 +0700

    feat : add seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'nama' => 'Superadmin',
+            'email' => 'superadmin@gmail.com',
+            'password' => Hash::make('password'),
+            'telephone' => '081234567890',
+            'role' => 'superadmin'
+        ]);
+    }
+}
