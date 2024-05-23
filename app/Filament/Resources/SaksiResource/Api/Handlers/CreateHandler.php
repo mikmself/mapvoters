e6e8b35fc1dc8859a9cf83fe1360622a55eaab33
mikmself <mikmself@gmail.com>
2024-05-24 01:00:06 +0700

    feat : saksi api, fix : detail exception message

--- app/Filament/Resources/SaksiResource/Api/Handlers/CreateHandler.php
+++ app/Filament/Resources/SaksiResource/Api/Handlers/CreateHandler.php
@@ -1,13 +1,20 @@
 <?php
 namespace App\Filament\Resources\SaksiResource\Api\Handlers;
 
+use App\Models\Saksi;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 use Rupadana\ApiService\Http\Handlers;
 use App\Filament\Resources\SaksiResource;
 
 class CreateHandler extends Handlers {
     public static string | null $uri = '/';
     public static string | null $resource = SaksiResource::class;
+
+    public static bool $public = true;
 
     public static function getMethod()
     {
@@ -20,12 +27,47 @@ class CreateHandler extends Handlers {
 
     public function handler(Request $request)
     {
-        $model = new (static::getModel());
-
-        $model->fill($request->all());
-
-        $model->save();
-
-        return static::sendSuccessResponse($model, "Successfully Create Resource");
+        try {
+            DB::beginTransaction();
+            $validator = Validator::make($request->all(), [
+                'tps' => 'required',
+                'provinsi_id' => 'required',
+                'kabupaten_id' => 'required',
+                'kecamatan_id' => 'required',
+                'kelurahan_id' => 'required',
+                'koordinator_id' => 'required',
+                'name' => 'required',
+                'email' => 'required|email|unique:users,email',
+                'password' => 'required',
+                'telephone' => 'required|numeric|unique:users,telephone',
+            ]);
+            if ($validator->fails()) {
+                return static::sendErrorResponse($validator->errors(), $validator->errors(), 422);
+            }
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+                'telephone' => $request->telephone,
+                'role' => 'saksi'
+            ]);
+            $newModel = Saksi::create([
+                'tps' => $request->tps,
+                'jumlah_suara' => $request->jumlah_suara,
+                'foto_kertas_suara' => $request->foto_kertas_suara != null ? $this->prosesFoto($request,'foto_kertas_suara') : null,
+                'provinsi_id' => $request->provinsi_id,
+                'kabupaten_id' => $request->kabupaten_id,
+                'kecamatan_id' => $request->kecamatan_id,
+                'kelurahan_id' => $request->kelurahan_id,
+                'koordinator_id' => $request->koordinator_id,
+                'user_id' => $user->id
+            ]);
+            $saksi = Saksi::where('id', $newModel->id)->with('provinsi', 'kabupaten', 'kecamatan', 'kelurahan', 'user', 'koordinator')->first();
+            DB::commit();
+            return static::sendSuccessResponse($saksi, 'Successfully Create Resource');
+        }catch (\Exception $e) {
+            DB::rollBack();
+            return static::sendErrorResponse($e->getMessage(), $e->getMessage(), 500);
+        }
     }
 }
