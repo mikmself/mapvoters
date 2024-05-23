72377abdc91ea8bd0b11f33f5768f0dac80472b4
mikmself <mikmself@gmail.com>
2024-05-23 15:59:06 +0700

    feat : pemetaan wilayah, fix : paslon api handlers

--- app/Filament/Resources/PaslonResource/Api/Handlers/CreateHandler.php
+++ app/Filament/Resources/PaslonResource/Api/Handlers/CreateHandler.php
@@ -1,13 +1,20 @@
 <?php
 namespace App\Filament\Resources\PaslonResource\Api\Handlers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 use Rupadana\ApiService\Http\Handlers;
 use App\Filament\Resources\PaslonResource;
 
 class CreateHandler extends Handlers {
     public static string | null $uri = '/';
     public static string | null $resource = PaslonResource::class;
+
+    public static bool $public = true;
+
 
     public static function getMethod()
     {
@@ -18,14 +25,46 @@ class CreateHandler extends Handlers {
         return static::$resource::getModel();
     }
 
+
     public function handler(Request $request)
     {
-        $model = new (static::getModel());
-
-        $model->fill($request->all());
-
-        $model->save();
-
-        return static::sendSuccessResponse($model, "Successfully Create Resource");
+        try {
+            DB::beginTransaction();
+            $validator = Validator::make($request->all(), [
+                'foto' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+                'type' => 'required',
+                'nomor_urut' => 'required',
+                'dapil' => 'required',
+                'partai_id' => 'required',
+                'name' => 'required',
+                'email' => 'required|email|unique:users,email',
+                'password' => 'required',
+                'telephone' => 'required|numeric|unique:users,telephone',
+            ]);
+            if ($validator->fails()) {
+                return static::sendErrorResponse($validator->errors(), $validator->errors(), 422);
+            }
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+                'telephone' => $request->telephone,
+                'role' => 'paslon'
+            ]);
+            $model = new (static::getModel());
+            $model->create([
+                'foto' => $this->prosesFoto($request,'paslon'),
+                'type' => $request->type,
+                'nomor_urut' => $request->nomor_urut,
+                'dapil' => $request->dapil,
+                'partai_id' => $request->partai_id,
+                'user_id' => $user->id
+            ]);
+            DB::commit();
+            return static::sendSuccessResponse($model, "Successfully Create Resource");
+        }catch (\Exception $e) {
+            DB::rollBack();
+            return static::sendErrorResponse($e->getMessage(), $e->getMessage(), 500);
+        }
     }
 }
---
+++ app/Http/Controllers/WilayahController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Kabupaten;
+use App\Models\Kecamatan;
+use App\Models\Kelurahan;
+use App\Models\Paslon;
+use App\Models\Provinsi;
+use Illuminate\Http\Request;
+
+class WilayahController extends Controller
+{
+    public function pemetaanProvinsi($idPaslon)
+    {
+        $paslon = Paslon::find($idPaslon);
+        $provinsi = Provinsi::whereHas('pemilihPotensial', function ($query) use ($idPaslon) {
+            $query->whereHas('koordinator', function ($query) use ($idPaslon) {
+                $query->where('paslon_id', $idPaslon);
+            });
+        })->withCount(['pemilihPotensial' => function ($query) use ($idPaslon) {
+            $query->whereHas('koordinator', function ($query) use ($idPaslon) {
+                $query->where('paslon_id', $idPaslon);
+            });
+        }])->get()->filter(function ($provinsi) {
+            return $provinsi->pemilih_potensial_count > 0;
+        });
+
+        return response()->json([
+            'message' => 'Data provinsi pemilih potensial paslon ' . $paslon->user->name . ' berhasil diambil',
+            'data' => $provinsi
+        ]);
+    }
+
+    public function pemetaanKabupaten($idPaslon)
+    {
+        $paslon = Paslon::find($idPaslon);
+        $kabupaten = Kabupaten::whereHas('pemilihPotensial', function ($query) use ($idPaslon) {
+            $query->whereHas('koordinator', function ($query) use ($idPaslon) {
+                $query->where('paslon_id', $idPaslon);
+            });
+        })->withCount(['pemilihPotensial' => function ($query) use ($idPaslon) {
+            $query->whereHas('koordinator', function ($query) use ($idPaslon) {
+                $query->where('paslon_id', $idPaslon);
+            });
+        }])->get()->filter(function ($kabupaten) {
+            return $kabupaten->pemilih_potensial_count > 0;
+        });
+
+        return response()->json([
+            'message' => 'Data kabupaten pemilih potensial paslon ' . $paslon->user->name . ' berhasil diambil',
+            'data' => $kabupaten
+        ]);
+    }
+
+    public function pemetaanKecamatan($idPaslon)
+    {
+        $paslon = Paslon::find($idPaslon);
+        $kecamatan = Kecamatan::whereHas('pemilihPotensial', function ($query) use ($idPaslon) {
+            $query->whereHas('koordinator', function ($query) use ($idPaslon) {
+                $query->where('paslon_id', $idPaslon);
+            });
+        })->withCount(['pemilihPotensial' => function ($query) use ($idPaslon) {
+            $query->whereHas('koordinator', function ($query) use ($idPaslon) {
+                $query->where('paslon_id', $idPaslon);
+            });
+        }])->get()->filter(function ($kecamatan) {
+            return $kecamatan->pemilih_potensial_count > 0;
+        });
+
+        return response()->json([
+            'message' => 'Data kecamatan pemilih potensial paslon ' . $paslon->user->name . ' berhasil diambil',
+            'data' => $kecamatan
+        ]);
+    }
+
+    public function pemetaanKelurahan($idPaslon)
+    {
+        $paslon = Paslon::find($idPaslon);
+        $kelurahan = Kelurahan::whereHas('pemilihPotensial', function ($query) use ($idPaslon) {
+            $query->whereHas('koordinator', function ($query) use ($idPaslon) {
+                $query->where('paslon_id', $idPaslon);
+            });
+        })->withCount(['pemilihPotensial' => function ($query) use ($idPaslon) {
+            $query->whereHas('koordinator', function ($query) use ($idPaslon) {
+                $query->where('paslon_id', $idPaslon);
+            });
+        }])->get()->filter(function ($kelurahan) {
+            return $kelurahan->pemilih_potensial_count > 0;
+        });
+
+        return response()->json([
+            'message' => 'Data kelurahan pemilih potensial paslon ' . $paslon->user->name . ' berhasil diambil',
+            'data' => $kelurahan
+        ]);
+    }
+
+}
