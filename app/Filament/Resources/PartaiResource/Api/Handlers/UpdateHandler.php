9aaeb6346ad5cc2001d876258e3ee8eb94417cfe
mikmself <mikmself@gmail.com>
2024-05-24 01:39:47 +0700

    feat : add api resource partai

--- app/Filament/Resources/PartaiResource/Api/Handlers/UpdateHandler.php
+++ app/Filament/Resources/PartaiResource/Api/Handlers/UpdateHandler.php
@@ -1,13 +1,17 @@
 <?php
 namespace App\Filament\Resources\PartaiResource\Api\Handlers;
 
+use App\Models\Partai;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Rupadana\ApiService\Http\Handlers;
 use App\Filament\Resources\PartaiResource;
 
 class UpdateHandler extends Handlers {
     public static string | null $uri = '/{id}';
     public static string | null $resource = PartaiResource::class;
+
+    public static bool $public = true;
 
     public static function getMethod()
     {
@@ -20,16 +24,31 @@ class UpdateHandler extends Handlers {
 
     public function handler(Request $request)
     {
-        $id = $request->route('id');
-
-        $model = static::getModel()::find($id);
-
-        if (!$model) return static::sendNotFoundResponse();
-
-        $model->fill($request->all());
-
-        $model->save();
-
-        return static::sendSuccessResponse($model, "Successfully Update Resource");
+        try {
+            DB::beginTransaction();
+            $id = $request->route('id');
+            $model = Partai::find($id);
+            if(!$model) {
+                $response = static::sendNotFoundResponse();
+            } else {
+                if($request->hasFile('logo')) {
+                    $logoPath = $this->prosesFoto($request, 'partai', 'logo');
+                } else {
+                    $logoPath = $model->logo;
+                }
+                $model->update([
+                    'nama' => $request->nama ? $request->nama : $model->nama,
+                    'nomor_urut' => $request->nomor_urut ? $request->nomor_urut : $model->nomor_urut,
+                    'logo' => $logoPath,
+                ]);
+                $partai = Partai::with('paslon')->find($id);
+                DB::commit();
+                $response = static::sendSuccessResponse($partai);
+            }
+        }catch(\Exception $e) {
+            DB::rollBack();
+            $response = static::sendErrorResponse($e->getMessage(), $e->getMessage(), 500);
+        }
+        return $response;
     }
 }
