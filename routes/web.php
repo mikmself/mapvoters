a22691df136b86055e1c0cdf6b4d7595f4e6299b
mikmself <mikmself@gmail.com>
2024-05-27 06:30:38 +0700

    feat : get wilayah anak berdasarkan wilayah induk

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,9 @@ Route::get('/api/pemetaan-provinsi/{idPaslon}', [\App\Http\Controllers\WilayahCo
 Route::get('/api/pemetaan-kabupaten/{idPaslon}', [\App\Http\Controllers\WilayahController::class,'pemetaanKabupaten']);
 Route::get('/api/pemetaan-kecamatan/{idPaslon}', [\App\Http\Controllers\WilayahController::class,'pemetaanKecamatan']);
 Route::get('/api/pemetaan-kelurahan/{idPaslon}', [\App\Http\Controllers\WilayahController::class,'pemetaanKelurahan']);
+
+#buatkan route untuk mengakses method get provinsi sampai kelurahan di wilayah controller
+Route::get('/api/get-provinsi', [\App\Http\Controllers\WilayahController::class, 'getProvinsi']);
+Route::get('/api/get-kabupaten-provinsi/{idProvinsi}', [\App\Http\Controllers\WilayahController::class, 'getKabupaten']);
+Route::get('/api/get-kecamatan-kabupaten/{idKabupaten}', [\App\Http\Controllers\WilayahController::class, 'getKecamatan']);
+Route::get('/api/get-kelurahan-kecamatan/{idKecamatan}', [\App\Http\Controllers\WilayahController::class, 'getKelurahan']);
