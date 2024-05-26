d712821a5edab0097b8cbcf429d842d191f9b945
mikmself <mikmself@gmail.com>
2024-05-26 21:05:56 +0700

    feat : add api resource provinsi

--- app/Filament/Resources/ProvinsiResource/Api/Handlers/CreateHandler.php
+++ app/Filament/Resources/ProvinsiResource/Api/Handlers/CreateHandler.php
@@ -1,13 +1,18 @@
 <?php
 namespace App\Filament\Resources\ProvinsiResource\Api\Handlers;
 
+use App\Models\Provinsi;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 use Rupadana\ApiService\Http\Handlers;
 use App\Filament\Resources\ProvinsiResource;
 
 class CreateHandler extends Handlers {
     public static string | null $uri = '/';
     public static string | null $resource = ProvinsiResource::class;
+
+    public static bool $public = true;
 
     public static function getMethod()
     {
@@ -20,12 +25,23 @@ class CreateHandler extends Handlers {
 
     public function handler(Request $request)
     {
-        $model = new (static::getModel());
-
-        $model->fill($request->all());
-
-        $model->save();
-
-        return static::sendSuccessResponse($model, "Successfully Create Resource");
+        try {
+            DB::beginTransaction();
+            $validator = Validator::make($request->all(), [
+                'name' => 'required',
+            ]);
+            if ($validator->fails()) {
+                return static::sendErrorResponse($validator->errors(), $validator->errors(), 422);
+            }
+            $newModel = static::getModel()::create([
+                'name' => $request->name,
+            ]);
+            $provinsi = Provinsi::where('id', $newModel->id)->first();
+            DB::commit();
+            return static::sendSuccessResponse($provinsi, 'Successfully Create Resource');
+        }catch (\Exception $e) {
+            DB::rollBack();
+            return static::sendErrorResponse($e->getMessage(), 'Failed to Create Resource', 500);
+        }
     }
 }
--- app/Filament/Resources/ProvinsiResource/Api/Handlers/UpdateHandler.php
+++ app/Filament/Resources/ProvinsiResource/Api/Handlers/UpdateHandler.php
@@ -1,13 +1,18 @@
 <?php
 namespace App\Filament\Resources\ProvinsiResource\Api\Handlers;
 
+use App\Models\Provinsi;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 use Rupadana\ApiService\Http\Handlers;
 use App\Filament\Resources\ProvinsiResource;
 
 class UpdateHandler extends Handlers {
     public static string | null $uri = '/{id}';
     public static string | null $resource = ProvinsiResource::class;
+
+    public static bool $public = true;
 
     public static function getMethod()
     {
@@ -20,16 +25,32 @@ class UpdateHandler extends Handlers {
 
     public function handler(Request $request)
     {
-        $id = $request->route('id');
-
-        $model = static::getModel()::find($id);
-
-        if (!$model) return static::sendNotFoundResponse();
-
-        $model->fill($request->all());
-
-        $model->save();
-
-        return static::sendSuccessResponse($model, "Successfully Update Resource");
+        try {
+            DB::beginTransaction();
+            $id = $request->route('id');
+            $model = Provinsi::find($id);
+            if (!$model) {
+                $response = static::sendNotFoundResponse();
+            } else {
+                $validator = Validator::make($request->all(), [
+                    'name' => 'sometimes|string',
+                ]);
+                if ($validator->fails()) {
+                    $response = static::sendErrorResponse($validator->errors(), $validator->errors(), 422);
+                } else {
+                    $model->update([
+                        'name' => $request->name ? $request->name : $model->name,
+                    ]);
+                    $provinsi = Provinsi::find($model->id);
+                    DB::commit();
+                    $response = static::sendSuccessResponse($provinsi,"Successfully Updated Resource");
+                }
+            }
+        }
+        catch (\Exception $e) {
+            DB::rollBack();
+            $response = static::sendErrorResponse($e->getMessage(), $e->getMessage(), 500);
+        }
+        return $response;
     }
 }
