4714e4895320e0985462f103f7c5d3617080ea78
mikmself <mikmself@gmail.com>
2024-05-26 21:51:09 +0700

    feat : add Kecamatan api Resource

--- app/Filament/Resources/KecamatanResource/Api/Handlers/CreateHandler.php
+++ app/Filament/Resources/KecamatanResource/Api/Handlers/CreateHandler.php
@@ -1,13 +1,18 @@
 <?php
 namespace App\Filament\Resources\KecamatanResource\Api\Handlers;
 
+use App\Models\Kecamatan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 use Rupadana\ApiService\Http\Handlers;
 use App\Filament\Resources\KecamatanResource;
 
 class CreateHandler extends Handlers {
     public static string | null $uri = '/';
     public static string | null $resource = KecamatanResource::class;
+
+    public static bool $public = true;
 
     public static function getMethod()
     {
@@ -20,12 +25,25 @@ class CreateHandler extends Handlers {
 
     public function handler(Request $request)
     {
-        $model = new (static::getModel());
-
-        $model->fill($request->all());
-
-        $model->save();
-
-        return static::sendSuccessResponse($model, "Successfully Create Resource");
+        try {
+            DB::beginTransaction();
+            $validator = Validator::make($request->all(),[
+                'nama' => 'required|string',
+                'kabupaten_id' => 'required',
+            ]);
+            if ($validator->fails()) {
+                return static::sendErrorResponse($validator->errors(), $validator->errors(), 422);
+            }
+            $newModel = Kecamatan::create([
+                'nama' => $request->name,
+                'kabupaten_id' => $request->kabupaten_id,
+            ]);
+            $kecamatan = Kecamatan::where('id', $newModel->id)->first();
+            DB::commit();
+            return static::sendSuccessResponse($kecamatan, 'Successfully Create Resource');
+        }catch (\Exception $e) {
+            DB::rollBack();
+            return static::sendErrorResponse($e->getMessage(), 'Failed to Create Resource', 500);
+        }
     }
 }
--- app/Filament/Resources/KecamatanResource/Api/Handlers/UpdateHandler.php
+++ app/Filament/Resources/KecamatanResource/Api/Handlers/UpdateHandler.php
@@ -1,13 +1,18 @@
 <?php
 namespace App\Filament\Resources\KecamatanResource\Api\Handlers;
 
+use App\Models\Kecamatan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 use Rupadana\ApiService\Http\Handlers;
 use App\Filament\Resources\KecamatanResource;
 
 class UpdateHandler extends Handlers {
     public static string | null $uri = '/{id}';
     public static string | null $resource = KecamatanResource::class;
+
+    public static bool $public = true;
 
     public static function getMethod()
     {
@@ -20,16 +25,31 @@ class UpdateHandler extends Handlers {
 
     public function handler(Request $request)
     {
-        $id = $request->route('id');
-
-        $model = static::getModel()::find($id);
-
-        if (!$model) return static::sendNotFoundResponse();
-
-        $model->fill($request->all());
-
-        $model->save();
-
-        return static::sendSuccessResponse($model, "Successfully Update Resource");
+        try{
+            DB::beginTransaction();
+            $id = $request->route('id');
+            $model = static::getModel()::find($id);
+            if (!$model) {
+                $response = static::sendNotFoundResponse();
+            } else {
+                $validator = Validator::make($request->all(), [
+                    'nama' => 'sometimes|string',
+                ]);
+                if ($validator->fails()) {
+                    $response = static::sendErrorResponse($validator->errors(), $validator->errors(), 422);
+                } else {
+                    $model->update([
+                        'nama' => $request->nama ? $request->nama : $model->nama,
+                    ]);
+                    $model = Kecamatan::find($model->id);
+                    DB::commit();
+                    $response = static::sendSuccessResponse($model,"Successfully Updated Resource");
+                }
+            }
+        }catch (\Exception $e) {
+            DB::rollBack();
+            $response = static::sendErrorResponse($e->getMessage(), $e->getMessage(), 500);
+        }
+        return $response;
     }
 }
